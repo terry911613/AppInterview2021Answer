e1c53a7648a2a96e096b8011be8c2c482f7830c8
TaiYiLee <terry911613@gmail.com>
2021-05-14 15:09:49 +0800

    Initial Commit

---
+++ AppInterview2021Answer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppInterview2021Answer
+//
+//  Created by 李泰儀 on 2021/5/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
